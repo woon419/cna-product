1f7258c26233ff60ab356fdc95d82032d9d40999
JJongMani <3827925@naver.com>
2020-05-28 15:17:16 +0900

    first commmit

---
+++ src/main/java/com/example/product/Product.java
@@ -0,0 +1,64 @@
+package com.example.product;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.persistence.*;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.util.MimeTypeUtils;
+
+ @Entity
+ public class Product {
+
+     @PostPersist
+     public void eventPublish(){
+         ProductChanged productChanged = new ProductChanged();
+         productChanged.setProductId(this.getId());
+         productChanged.setProductName(this.getName());
+         productChanged.setProductStock(this.getStock());
+         ObjectMapper objectMapper = new ObjectMapper();
+         String json = null;
+
+         try {
+             json = objectMapper.writeValueAsString(productChanged);
+         } catch (JsonProcessingException e) {
+             throw new RuntimeException("JSON format exception", e);
+         }
+
+         Processor processor = ProductApplication.applicationContext.getBean(Processor.class);
+         MessageChannel outputChannel = processor.output();
+
+         outputChannel.send(MessageBuilder
+                 .withPayload(json)
+                 .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
+                 .build());
+     }
+
+     @Id @GeneratedValue
+     Long id;
+     String name;
+     int stock;
+     public Long getId() {
+         return id;
+     }
+     public void setId(Long id) {
+         this.id = id;
+     }
+     public String getName() {
+         return name;
+     }
+     public void setName(String name) {
+         this.name = name;
+        }
+
+        public int getStock() {
+            return stock;
+        }
+
+        public void setStock(int stock) {
+            this.stock = stock;
+        }
+    }
+
